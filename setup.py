dd43dd298ae4677864cbb01221c99777f8e0e59a
Saumya Saksena <saumya.saksena@gitlab.navya.tech>
2025-10-24 13:52:59 +0200

    feat: Add basic files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Setup script for VisDrone Toolkit.
+
+This file is for backward compatibility. Modern installation uses pyproject.toml.
+"""
+
+from setuptools import setup
+
+# All configuration is in pyproject.toml
+# This file exists for backward compatibility with older pip versions
+setup()
